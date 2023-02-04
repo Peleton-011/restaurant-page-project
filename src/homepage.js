15d944c6c012825d8b28b716c5c8d188db6d2f8b
Nico <nicodiazan@gmail.com>
2023-02-04 20:39:35 +0100

    Move homepage generation to its own file

---
+++ src/homepage.js
@@ -0,0 +1,17 @@
+function component() {
+    const element = document.createElement("div");
+    const btn = document.createElement("button");
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+    element.appendChild(btn);
+
+
+    return element;
+}
+
+export {
+    component,
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import _ from "lodash";
 
-function component() {
-    const element = document.createElement("div");
-    const btn = document.createElement("button");
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-    element.appendChild(btn);
-
-
-    return element;
-}
-
 document.body.appendChild(component());
