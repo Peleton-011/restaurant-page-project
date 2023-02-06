076de1eb83efbfbd56686085cdf7de45a171ecc1
Nico <nicodiazan@gmail.com>
2023-02-06 14:03:08 +0100

    Add basic styling and a couple more pages

---
+++ src/pageData/about.js
@@ -0,0 +1,17 @@
+function component() {
+    const element = document.createElement("div");
+    const btn = document.createElement("button");
+
+    // Lodash, now imported by this script
+    element.innerHTML = "Test";
+
+    element.appendChild(btn);
+
+    return element;
+}
+
+function main(parent) {
+    parent.appendChild(component());
+}
+
+export { main };
