3a5d6437a86a9b5fd2c659377df4d319b19b05bb
Nico <nicodiazan@gmail.com>
2023-04-14 21:21:08 +0200

    Improve some of the styling

--- src/pageData/homepage.js
+++ src/pageData/homepage.js
@@ -2,17 +2,26 @@ import restaurantPic from "../assets/images/restaurant-img.jpeg";
 
 function component() {
 	const element = document.createElement("div");
+    element.className = "container bg-img";
 
-	element.style.background = `url(${restaurantPic})`;
+	element.style.backgroundImage = `url(${restaurantPic})`;
 
 	const header = document.createElement("h2");
+
 
 	header.innerHTML = `The perfect stop in all of Space Highway 
         <span style="filter: hue-rotate(-30deg)">011</span>
          - 
         <span style="filter: hue-rotate(-30deg)">\u00A713</span>`;
-
+    header.className = "glowy-text";
 	element.appendChild(header);
+
+    //______
+
+    const desc = document.createElement("article");
+
+    desc.innerHTML = ``
+
 	return element;
 }
 
