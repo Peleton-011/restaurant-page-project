7535bdc7db45ce4db3611dcc3a8ad0aa890c60b8
Nico <nicodiazan@gmail.com>
2023-04-15 14:34:11 +0200

    Improve styles

--- src/pageData/homepage.js
+++ src/pageData/homepage.js
@@ -2,32 +2,35 @@ import restaurantPic from "../assets/images/restaurant-img.jpeg";
 
 function component() {
 	const element = document.createElement("div");
-    element.className = "container bg-img";
+	element.className = "container bg-img";
 
 	element.style.backgroundImage = `url(${restaurantPic})`;
 
-
-    const card = document.createElement("section");
-    card.className = "img-card";
+	const card = document.createElement("section");
+	card.className = "img-card";
 
 	const title = document.createElement("h2");
 
-
-	title.innerHTML = `The best stop in all of Spaceway
+	title.innerHTML = `The Best Stop in all of Spaceway
         X - <span style="filter: hue-rotate(-30deg)">\u00A713</span>`;
-    title.className = "glowy-text";
+	title.className = "glowy-text";
 	card.appendChild(title);
 
-    //______
+	//______
 
-    const desc = document.createElement("p");
-    desc.className = "text-shadow";
+	const desc = document.createElement("p");
+	desc.className = "text-shadow";
 
-    desc.innerHTML = `Testy test for testing purposes`
+	desc.innerHTML = `
+    Hydrogen-3 running out?<br><br>
+    Need a nice meal to take a breather?<br><br>
+    Want a drink while you wait for your FTL drive to cool down?<br><br>
+    <strong><em style="filter: hue-rotate(-30deg); font-size: 1.5rem">We've Got You Covered!</em></strong><br><br>
+    `;
 
-    card.appendChild(desc);
+	card.appendChild(desc);
 
-    element.appendChild(card);
+	element.appendChild(card);
 
 	return element;
 }
