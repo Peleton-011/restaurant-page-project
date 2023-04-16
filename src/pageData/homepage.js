4e470338aaaf960c7442f9390378512471a444b7
Nico <nicodiazan@gmail.com>
2023-04-16 17:15:35 +0200

    Refactor description generation

--- src/pageData/homepage.js
+++ src/pageData/homepage.js
@@ -2,21 +2,39 @@ import restaurantPic from "../assets/images/restaurant-img.jpeg";
 
 import content from "./pageContent/homepage.json";
 
-function makeHeader(title) {
+function makeHeader(titleData) {
 	const titles = document.createElement("hgroup");
 	const en = document.createElement("h2");
 
-	en.innerHTML = title.en;
+	en.innerHTML = titleData.en;
 	en.className = "glowy-text";
 	titles.appendChild(en);
 
 	const jp = document.createElement("h2");
 
-	jp.innerHTML = title.jp;
+	jp.innerHTML = titleData.jp;
 	jp.className = "glowy-text";
 	titles.appendChild(jp);
 
 	return titles;
+}
+
+function makeDescription(descData) {
+	const desc = document.createElement("p");
+	desc.className = "text-shadow";
+
+	const descHTML = descData.reduce((acc, item) => {
+		acc += item.en + "<br>";
+		acc += `<span class="jap-text">${item.jp}</span>` + "<br><br>";
+		return acc;
+	}, "");
+
+	desc.innerHTML = descHTML;
+	
+	//We've Got You Covered!
+	//私たちは、あなたをサポートしています！
+
+	return desc;
 }
 
 function card({ callToAction }) {
@@ -26,34 +44,12 @@ function card({ callToAction }) {
 	card.className = "img-card";
 
 	//Titles for the card over image
-	const title = makeHeader(cardData.title)
+	const title = makeHeader(cardData.title);
 	card.appendChild(title);
 
 	//______
 
-	const desc = document.createElement("p");
-	desc.className = "text-shadow";
-
-	desc.innerHTML = `
-    <br>
-	<span class="jap-text"></span><br><br>
-    <br>
-	<span class="jap-text"></span><br><br>
-    <br>
-	<span class="jap-text"></span><br><br>
-    	<strong>
-			<button class="link">	
-				<em class="highlight"></em> 
-			</button>	
-			&nbsp;&nbsp;|&nbsp;&nbsp;
-			<button class="link">	
-				<span class="jap-text highlight"></span>
-			</button>	
-		</strong>
-	<br><br>
-    `;
-	//We've Got You Covered!
-	//私たちは、あなたをサポートしています！
+	const desc = makeDescription(cardData.desc);
 
 	card.appendChild(desc);
 
