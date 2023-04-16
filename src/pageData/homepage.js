8f10033a02cded02efc9de035ed2dc12100847dd
Nico <nicodiazan@gmail.com>
2023-04-16 12:19:59 +0200

    Add a japanese version of all content and improve styles

--- src/pageData/homepage.js
+++ src/pageData/homepage.js
@@ -1,20 +1,33 @@
 import restaurantPic from "../assets/images/restaurant-img.jpeg";
 
 function component() {
-	const element = document.createElement("div");
-	element.className = "container bg-img";
+	const page = document.createElement("div");
+	page.className = "container bg-img";
 
-	element.style.backgroundImage = `url(${restaurantPic})`;
+	page.style.backgroundImage = `url(${restaurantPic})`;
 
 	const card = document.createElement("section");
 	card.className = "img-card";
+
+	//Titles for the card over image
+
+	const titles = document.createElement("hgroup");
 
 	const title = document.createElement("h2");
 
 	title.innerHTML = `The Best Stop in all of Spaceway
         X - <span style="filter: hue-rotate(-30deg)">\u00A713</span>`;
 	title.className = "glowy-text";
-	card.appendChild(title);
+	titles.appendChild(title);
+
+	const japTitle = document.createElement("h2");
+
+	japTitle.innerHTML = `スペースウェイX - <span style="filter: hue-rotate(-30deg)">\u00A713 </span>のベストストップ`
+	japTitle.className = "glowy-text";
+	titles.appendChild(japTitle);
+
+	card.appendChild(titles);
+
 
 	//______
 
@@ -22,17 +35,21 @@ function component() {
 	desc.className = "text-shadow";
 
 	desc.innerHTML = `
-    Hydrogen-3 running out?<br><br>
-    Need a nice meal to take a breather?<br><br>
-    Want a drink while you wait for your FTL drive to cool down?<br><br>
-    <strong><em style="filter: hue-rotate(-30deg); font-size: 1.5rem">We've Got You Covered!</em></strong><br><br>
+    Hydrogen-3 running out?<br>
+	<span class="jap-text">水素3が不足する？</span><br><br>
+    Need a nice meal to take a breather?<br>
+	<span class="jap-text">ほっと一息つくのに美味しい食事が必要？</span><br><br>
+    Want a drink while you wait for your FTL drive to cool down?<br>
+	<span class="jap-text">光より速いドライブが冷めるのを待つ間、飲み物はいかがですか？</span><br><br>
+    <strong><em class="highlight">We've Got You Covered!</em> &nbsp;&nbsp;|&nbsp;&nbsp;
+	<span class="jap-text highlight">私たちは、あなたをサポートしています！</span></strong><br><br>
     `;
 
 	card.appendChild(desc);
 
-	element.appendChild(card);
+	page.appendChild(card);
 
-	return element;
+	return page;
 }
 
 function main(parent) {
